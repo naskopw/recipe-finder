6abbfa3426f48d4333abb50c2d5ce062822469ae
naskopw <naskopw@yahoo.com>
2021-08-31 09:06:22 +0300

    Extract BASE_API to an environment variable

--- client/src/Services/RecipeService.js
+++ client/src/Services/RecipeService.js
@@ -1,29 +1,31 @@
 import axios from "axios"
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+
 export async function getTrending() {
-    const API_URL = "http://localhost:3000/api/recipes/trending"
+    const API_URL = `${API_BASE_URL}/api/recipes/trending`
     let response = await axios.get(API_URL)
     return response.data
 }
 export async function getRecipe(id) {
-    const API_URL = `http://localhost:3000/api/recipes/${id}`
+    const API_URL = `${API_BASE_URL}/api/recipes/${id}`
     let response = await axios.get(API_URL)
     return response.data
 }
 
 export async function getCategory(id) {
-    const API_URL = `http://localhost:3000/api/categories/${id}`
+    const API_URL = `${API_BASE_URL}/api/categories/${id}`
     let response = await axios.get(API_URL)
     return response.data
 }
 export async function getCategoryByStart(start) {
-    const API_URL = `http://localhost:3000/api/categories/?startsWith=${start}`
+    const API_URL = `${API_BASE_URL}/api/categories/?startsWith=${start}`
     let response = await axios.get(API_URL)
     return response.data
 }
 
 export async function getAllCategories() {
-    const API_URL = `http://localhost:3000/api/categories`
+    const API_URL = `${API_BASE_URL}/api/categories`
     let response = await axios.get(API_URL)
     return response.data
 }
