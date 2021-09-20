114514062430a274ca11e40d9d42758b45ed8811
naskopw <naskopw@yahoo.com>
2021-09-20 15:43:02 +0300

    Chefs added

--- client/src/Components/Home/Chefs/Chefs.js
+++ client/src/Components/Home/Chefs/Chefs.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import "./style.css"
 
 const Chefs = () => {
     return (
-        <section className="section-chefs"/>
+        <section id="section-chefs" className="text-center">
+            <div className="container-fluid">
+                <div className="col-md-12 col-md-offset-1 section-title">
+                    <h2>Meet the Chefs</h2>
+                    <hr/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 team">
+                    <div className="thumbnail">
+                        <div className="team-img"><img src={"/img/team/02.jpg"} alt="..."/></div>
+                        <div className="caption">
+                            <h3>Atanas Ivanov</h3>
+                            <p>Internet Software Technologies, NBU, f98757</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 team">
+                    <div className="thumbnail">
+                        <div className="team-img"><img src={"/img/team/01.jpg"} alt="..."/></div>
+                        <div className="caption">
+                            <h3>Philip Yankov</h3>
+                            <p>Mentor</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
