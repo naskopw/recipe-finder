e1856d7b98fef3a0aec1be8ddea9411b87477a0b
naskopw <naskopw@yahoo.com>
2022-03-12 02:15:52 -0800

    Seed spring security roles

--- src/main/java/com/recipefinder/recipefinder/services/DBSeedService.java
+++ src/main/java/com/recipefinder/recipefinder/services/DBSeedService.java
@@ -5,8 +5,11 @@ import com.recipefinder.recipefinder.bootstrap.ParsedRecipe;
 import com.recipefinder.recipefinder.bootstrap.ParsedRecipeMapper;
 import com.recipefinder.recipefinder.models.Category;
 import com.recipefinder.recipefinder.models.Recipe;
+import com.recipefinder.recipefinder.models.authentication.ERole;
+import com.recipefinder.recipefinder.models.authentication.Role;
 import com.recipefinder.recipefinder.repository.CategoryRepository;
 import com.recipefinder.recipefinder.repository.RecipeRepository;
+import com.recipefinder.recipefinder.repository.authentication.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +27,8 @@ public class DBSeedService {
     private RecipeRepository recipeRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     private void loadRecipesFromJson() {
         File file = new File(RECIPE_BOOTSTRAP_FILE);
@@ -46,6 +51,15 @@ public class DBSeedService {
         }
         recipeRepository.saveAll(allRecipes);
         categoryRepository.saveAll(mapCategoriesToRecipes(allRecipes, allCategories));
+        seedRoles();
+    }
+
+    private void seedRoles() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new Role(ERole.ROLE_USER));
+        roles.add(new Role(ERole.ROLE_ADMIN));
+        roles.add(new Role(ERole.ROLE_MODERATOR));
+        roleRepository.saveAll(roles);
     }
 
     private Recipe createRecipe(ParsedRecipe parsedRecipe) {
